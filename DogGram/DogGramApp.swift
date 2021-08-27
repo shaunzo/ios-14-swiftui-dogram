8460202bc22c5c710749da5ee23eb06da7ad3374
Shaun Reddiar <shaunreddiar@gmail.com>
2021-08-27 21:14:16 +0200

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Shaun Reddiar on 2021/08/27.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
