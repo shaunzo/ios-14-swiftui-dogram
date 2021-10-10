f7a46a2db38d9765626f14dfb2bbc7bb3926c8fe
Shaun Reddiar <shaunreddiar@gmail.com>
2021-10-10 12:47:31 +0200

    Settings Screen - added Settings Label SubView

---
+++ DogGram/Views/SubViews/SettingsLabelView.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsLabelView.swift
+//  DogGram
+//
+//  Created by Shaun Reddiar on 2021/10/10.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    var labelText: String
+    var labelImage: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(labelText)
+                    .fontWeight(.bold)
+                Spacer()
+                Image(systemName: labelImage)
+            }
+            
+            Divider()
+                .padding(.vertical, 4)
+        }
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Test Label", labelImage: "heart")
+            .previewLayout(.sizeThatFits)
+    }
+}
