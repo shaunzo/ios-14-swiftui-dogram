d86b81347369af24858cc8575b7056ee6823c05b
Shaun Reddiar <shaunreddiar@gmail.com>
2021-10-10 13:12:18 +0200

    Settings Screen - Created static layout for SettingsRowView

--- DogGram/Views/SubViews/SettingsRowView.swift
+++ DogGram/Views/SubViews/SettingsRowView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct SettingsRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(Color.blue)
+                Image(systemName: "heart.fill")
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }
+            .frame(width: 36, height: 36, alignment: .center)
+            Text("Row title")
+            Spacer()
+            Image(systemName: "chevron.right")
+                .font(.headline)
+        }
+        .padding(.vertical, 4)
     }
 }
 
