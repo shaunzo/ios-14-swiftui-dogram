9f93424c4fa2624433efaa50631983abc4f495f3
Shaun Reddiar <shaunreddiar@gmail.com>
2021-10-10 12:22:25 +0200

    Settings Screen - Added Swift UI file

---
+++ DogGram/Views/Screens/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  DogGram
+//
+//  Created by Shaun Reddiar on 2021/10/10.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
