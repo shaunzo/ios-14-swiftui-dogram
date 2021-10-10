31b27bb5b1035ec0bd635601e20de1fe291ae097
Shaun Reddiar <shaunreddiar@gmail.com>
2021-10-10 13:16:53 +0200

    Settings Screen - Made SettingsRowView fields reusable

--- DogGram/Views/SubViews/SettingsRowView.swift
+++ DogGram/Views/SubViews/SettingsRowView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct SettingsRowView: View {
+    var leftIcon: String
+    var text: String
+    var color: Color
     var body: some View {
         HStack {
             ZStack {
                 RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .fill(Color.blue)
-                Image(systemName: "heart.fill")
+                    .fill(color)
+                Image(systemName: leftIcon)
                     .font(.title3)
                     .foregroundColor(.white)
             }
             .frame(width: 36, height: 36, alignment: .center)
-            Text("Row title")
+            Text(text)
             Spacer()
             Image(systemName: "chevron.right")
                 .font(.headline)
@@ -29,7 +32,7 @@ struct SettingsRowView: View {
 
 struct SettingsRowView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsRowView()
+        SettingsRowView(leftIcon: "heart.fill", text: "Row Title", color: .red)
             .previewLayout(.sizeThatFits)
     }
 }
